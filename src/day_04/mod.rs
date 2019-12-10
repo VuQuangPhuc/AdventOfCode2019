8fb5b0e45103955de12ac48e170133569d5c8351
Vu Quang Phuc <phuc.vu-quang@gmx.de>
2019-12-10 15:35:22 +0100

    refactored day 4/1 to not use brute force

--- src/day_04/mod.rs
+++ src/day_04/mod.rs
@@ -1,42 +1,64 @@
 use std::collections::HashSet;
 
 pub fn solve() {
-    println!("Solution 1: {:?}", p_one());
-    println!("Solution 2: {:?}", p_two());
+    println!("Solution 1: {:?}", not_brute_force());
 }
 
-fn p_one() -> i32 {
-    brute_force()
-}
 
 fn p_two() -> i32 {
     more_brute_force()
 }
 
-fn brute_force() -> i32 {
+fn not_brute_force() -> i32 {
+    let limit = 576723;
     let mut hits = 0;
-    for i in 109165..576724 {
-        let arr: Vec<u32> = i
+    let mut password = 109165;
+    while password < limit {
+        let mut digits: Vec<u32> = password
             .to_string()
             .chars()
             .map(|c| c.to_digit(10).unwrap())
             .collect();
-        let mut not_decreasing = true;
-        let mut adjacent = false;
-        for j in 0..5 {
-            if arr[j] > arr[j + 1] {
-                not_decreasing = false;
-            }
-            if arr[j] == arr[j + 1] {
-                adjacent = true;
-            }
+        make_inc(&mut digits);
+        password = back(&digits);
+        if password > limit {
+            break;
         }
-        if not_decreasing && adjacent {
+        if has_double(&digits) {
             hits = hits + 1;
         }
+        password = password + 1;
     }
 
     hits
+}
+
+fn make_inc(digits: &mut Vec<u32>) {
+    for i in (1..6).rev() {
+        if digits[i] < digits[i - 1] {
+            for j in i..6 {
+                digits[j] = digits[i - 1];
+            }
+        }
+    }
+}
+
+fn has_double(digits: &Vec<u32>) -> bool {
+    for i in 0..5 {
+        if digits[i] == digits[i + 1] {
+            return true;
+        };
+    }
+    false
+}
+
+fn back(digits: &Vec<u32>) -> u32 {
+    digits[0] * 100000
+        + digits[1] * 10000
+        + digits[2] * 1000
+        + digits[3] * 100
+        + digits[4] * 10
+        + digits[5]
 }
 
 fn more_brute_force() -> i32 {
@@ -57,7 +79,7 @@ fn more_brute_force() -> i32 {
             if arr[j] == arr[j + 1] {
                 if j != 5 && seen.insert(arr[j]) {
                     let mut count = 1;
-                    for k in j + 1..6{
+                    for k in j + 1..6 {
                         if arr[k] == arr[j] {
                             count = count + 1;
                         }
